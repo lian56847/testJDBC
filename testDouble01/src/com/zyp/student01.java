0c6f07185a1a4fec94084882141b8f72c7d82a2b
wei <3265613778@qq.com>
2022-11-26 01:44:18 +0800

    田伟的小号修改了student01.java

--- testDouble01/src/com/zyp/student01.java
+++ testDouble01/src/com/zyp/student01.java
@@ -12,5 +12,7 @@ public class student01 {
     //这是一个main方法，是程序的入口:
     public static void main(String[] args) {
         System.out.println("args = " + Arrays.deepToString(args));
+		
+		aaaaa
     }
 }
