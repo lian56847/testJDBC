0b1ec7275c649dec430f2c67ef8cc4b9bae29ba7
wei <3265613778@qq.com>
2022-11-25 23:35:36 +0800

    这是田伟第二次提交

--- testDouble01/src/com/zyp/student01.java
+++ testDouble01/src/com/zyp/student01.java
@@ -1,5 +1,7 @@
 package com.zyp;
 
+import java.util.Arrays;
+
 /**
  * @Auther: TianWei_18811580953
  * @Date: 2022/11/25 - 11 - 25 - 23:23
@@ -7,4 +9,8 @@ package com.zyp;
  * @version: 1.0
  */
 public class student01 {
+    //这是一个main方法，是程序的入口:
+    public static void main(String[] args) {
+        System.out.println("args = " + Arrays.deepToString(args));
+    }
 }
