e09a051797e344214a9d794184cdca012420fe46
wei <3265613778@qq.com>
2022-11-26 00:10:35 +0800

    这是田伟第四次提交

--- testDouble01/src/com/zyp/Person.java
+++ testDouble01/src/com/zyp/Person.java
@@ -1,5 +1,8 @@
 package com.zyp;
 
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
 /**
  * @Auther: TianWei_18811580953
  * @Date: 2022/11/25 - 11 - 25 - 23:57
@@ -7,4 +10,8 @@ package com.zyp;
  * @version: 1.0
  */
 public class Person {
+    //这是一个main方法，是程序的入口:
+    public static void main(String[] args) {
+        System.out.println("args = " + Arrays.deepToString(args));
+    }
 }
